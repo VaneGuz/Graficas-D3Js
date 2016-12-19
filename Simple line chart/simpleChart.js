c5c4e9f94b4386db67804b6a7929b82e09c01413
vaneguz <vanesa.guzman@udea.edu.co>
2016-12-19 16:12:10 -0500

    nuevas gráficas

--- Simple line chart/simpleChart.js
+++ Simple line chart/simpleChart.js
@@ -3,24 +3,29 @@ nv.addGraph(function() {
   var chart = nv.models.lineChart()
                 .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
                 .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
-                .transitionDuration(350)  //how fast do you want the lines to transition?
+                //.transitionDuration(350)  //how fast do you want the lines to transition?
                 .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
                 .showYAxis(true)        //Show the y-axis
                 .showXAxis(true)        //Show the x-axis
+                .focusEnable(true);
   ;
 
   chart.xAxis     //Chart x-axis settings
       .axisLabel('Time (ms)')
-      .tickFormat(d3.format(',r'));
+      .tickFormat(d3.format(',f'));
 
   chart.yAxis     //Chart y-axis settings
       .axisLabel('Voltage (v)')
-      .tickFormat(d3.format('.02f'));
+      .tickFormat(d3.format('.2f'));
+
+  chart.y2Axis     //Chart y-axis settings
+
+          .tickFormat(d3.format('.2f'));
 
   /* Done setting the chart up? Time to render it!*/
   var myData = sinAndCos();   //You need data...
 
-  d3.select('#chart svg')    //Select the <svg> element you want to render the chart in.   
+  d3.select('#chart svg')    //Select the <svg> element you want to render the chart in.
       .datum(myData)         //Populate the <svg> element with chart data...
       .call(chart);          //Finally, render the chart!
 
